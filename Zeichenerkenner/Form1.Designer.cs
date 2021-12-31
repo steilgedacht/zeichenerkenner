5aad8a1f469a20da7c8503f080b5c55eed1e3d58
Benjamin Bergmann <89748204+steilgedacht@users.noreply.github.com>
2021-12-31 13:32:02 +0100

    Add files via upload

---
+++ Zeichenerkenner/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace Zeichenerkenner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cleanbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.progressBar7 = new System.Windows.Forms.ProgressBar();
+            this.progressBar8 = new System.Windows.Forms.ProgressBar();
+            this.progressBar9 = new System.Windows.Forms.ProgressBar();
+            this.progressBar10 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cleanbutton
+            // 
+            this.Cleanbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.4F);
+            this.Cleanbutton.Location = new System.Drawing.Point(336, 0);
+            this.Cleanbutton.Name = "Cleanbutton";
+            this.Cleanbutton.Size = new System.Drawing.Size(69, 30);
+            this.Cleanbutton.TabIndex = 0;
+            this.Cleanbutton.Text = "Clean";
+            this.Cleanbutton.UseVisualStyleBackColor = true;
+            this.Cleanbutton.Click += new System.EventHandler(this.Cleanbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(337, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 336);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(336, 30);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(69, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(336, 53);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(69, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(336, 76);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(69, 23);
+            this.progressBar3.TabIndex = 5;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(336, 99);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(69, 23);
+            this.progressBar4.TabIndex = 6;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(336, 122);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(69, 23);
+            this.progressBar5.TabIndex = 7;
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.Location = new System.Drawing.Point(336, 145);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(69, 23);
+            this.progressBar6.TabIndex = 8;
+            // 
+            // progressBar7
+            // 
+            this.progressBar7.Location = new System.Drawing.Point(336, 168);
+            this.progressBar7.Name = "progressBar7";
+            this.progressBar7.Size = new System.Drawing.Size(69, 23);
+            this.progressBar7.TabIndex = 9;
+            // 
+            // progressBar8
+            // 
+            this.progressBar8.Location = new System.Drawing.Point(336, 191);
+            this.progressBar8.Name = "progressBar8";
+            this.progressBar8.Size = new System.Drawing.Size(69, 23);
+            this.progressBar8.TabIndex = 10;
+            // 
+            // progressBar9
+            // 
+            this.progressBar9.Location = new System.Drawing.Point(336, 214);
+            this.progressBar9.Name = "progressBar9";
+            this.progressBar9.Size = new System.Drawing.Size(69, 23);
+            this.progressBar9.TabIndex = 11;
+            // 
+            // progressBar10
+            // 
+            this.progressBar10.Location = new System.Drawing.Point(336, 237);
+            this.progressBar10.Name = "progressBar10";
+            this.progressBar10.Size = new System.Drawing.Size(69, 24);
+            this.progressBar10.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(342, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(342, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(342, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(342, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(342, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(342, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(342, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "6";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(342, 194);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "7";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(342, 217);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "8";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Location = new System.Drawing.Point(342, 240);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "9";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(154, 341);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "Zentrieren";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Neuronales Netz neu trainieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 338);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(31, 20);
+            this.numericUpDown1.TabIndex = 26;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 361);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar10);
+            this.Controls.Add(this.progressBar9);
+            this.Controls.Add(this.progressBar8);
+            this.Controls.Add(this.progressBar7);
+            this.Controls.Add(this.progressBar6);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cleanbutton);
+            this.MaximumSize = new System.Drawing.Size(420, 400);
+            this.MinimumSize = new System.Drawing.Size(420, 400);
+            this.Name = "Form1";
+            this.Text = "Ziffernerkenner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Cleanbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+        private System.Windows.Forms.ProgressBar progressBar7;
+        private System.Windows.Forms.ProgressBar progressBar8;
+        private System.Windows.Forms.ProgressBar progressBar9;
+        private System.Windows.Forms.ProgressBar progressBar10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Zeichenerkenner/Form1.cs
@@ -0,0 +1,214 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Zeichenerkenner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Mousegedrückt = false; // Varialbe die beschreibt ob gerade gezeichnet werden soll oder nicht.
+        Bitmap bitmap = new Bitmap(336, 336);
+        Neuronales_Netzwerk NN = new Neuronales_Netzwerk(28 * 28, 100, 10, 0.1);
+        double[] Pixelarray = new double[784];
+
+        private void Form1_Load(object sender, EventArgs e) // Programm beim Start aufsetzen, eigentlich soll nur die gesamte Leinwand auf weiß gesetzt werden
+        {
+            CleanBox();
+        }
+
+        private void CleanBox() //Funktion die jeden Pixel auf der Zeichenfläche auf fast weiß zu setzen
+        {
+            for (int i = 0; i < 336; i++)
+            {
+                for (int j = 0; j < 336; j++)
+                {
+                    bitmap.SetPixel(i, j, Color.FromArgb(254, 254, 254)); // es wird nicht komplett weiß gesetzt, da im späteren Codeverlauf sich ein Rundungsfehler bei der Durchschnittberechenung einschleichen würde.
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Cleanbutton_Click(object sender, EventArgs e) // Button der die Zeichenfläche resetet
+        {
+            CleanBox();
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e) // Wenn die Mouse über der Picturebox bewegt werden soll soll gezeichnet werden
+        {
+            if (Mousegedrückt == true)
+            {
+                bmpzeichnen(e.X, e.Y);
+                pictureBox1.Image = bitmap;
+            }
+        }
+
+        private void bmpzeichnen(int positionx, int positiony) // Die Funktion, die einen 21x21 fetten Fleck auf die Leinwand malt.
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                for (int j = 0; j < 21; j++)
+                {
+                    if (positionx - 10 + i > 0 && positionx - 10 + i < 336) // Die Leinwand warf nach außen nicht überschritten werden
+                    {
+                        if (positiony - 10 + j > 0 && positiony - 10 + j < 336)// Die Leinwand warf nach außen nicht überschritten werden
+                        {
+                            bitmap.SetPixel(positionx - 10 + i, positiony - 10 + j, Color.Black);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e) // Wenn die Maus gedrückt wird
+        {
+            Mousegedrückt = true;
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e) // Wenn die Maus wieder losgelassen wird
+        {
+            Mousegedrückt = false;
+            Bitmapverpixeln();
+            Netzabfrage();
+        }
+
+        private void Bitmapverpixeln() // Pixelalgorytmus
+        {
+            Bitmap Pixelbmp = new Bitmap(336, 336);
+
+            if (checkBox1.Checked == true)
+            {
+                Bitmap bitmapplaceholder = new Bitmap(336, 336);
+                int ptop = 0, pright = 0, pleft = 0, pbottom = 0, xverschiebung, yverschiebung; // Dieser Algorythmus ist dazu da das gezeichntete Bidl in dei Mitte zu verscheiben.
+                for (int i = 0; i < (336); i++)
+                {
+                    for (int j = 0; j < 336; j++)
+                    {
+                        if (bitmap.GetPixel(i, j).R < 240 && pleft == 0)//Hier werden jeweils die Top Bottom Left und Right Werte einer gedachten Umrandung des gezeichneten Zeichens ausgelesen
+                        {
+                            pleft = i + 3;
+                        }
+                        if (bitmap.GetPixel(j, i).R < 240)
+                        {
+                            pright = j - 3;
+                        }
+                        if (bitmap.GetPixel(j, i).R < 240 && ptop == 0)
+                        {
+                            ptop = i + 3;
+                        }
+                        if (bitmap.GetPixel(j, i).R < 240)
+                        {
+                            pbottom = i -3;
+                        }
+                    }
+                }
+                xverschiebung = (int)((pleft + (pright - pleft) / 2) - 336 / 2); // hier wird die Entfernung die zu verschieben ist in x richtung berechnet, sodass das Gezeichnete miitttig ist.
+                yverschiebung = (int)((ptop + (pbottom - ptop) / 2) - 336 / 2); // hier wird die Entfernung in y richtung die zu verschieben ist, sodass das Gezeichnete mittig ist berchnet.
+
+                for (int i = 0; i < 336; i++) // die bitmap kann nur so kopiert werden ohne fehler zu verursachen
+                {
+                    for (int j = 0; j < 336; j++)
+                    {
+                        bitmapplaceholder.SetPixel(i, j, bitmap.GetPixel(i, j));
+                    }
+                }
+
+                CleanBox();
+                for (int i = 0; i < 336; i++)
+                {
+                    for (int j = 0; j < 336; j++)
+                    {
+                        if (bitmapplaceholder.GetPixel(j, i).R < 250)
+                        {
+                            bitmap.SetPixel(j - xverschiebung, i - yverschiebung, bitmapplaceholder.GetPixel(j, i));
+                        }
+                        bitmapplaceholder.SetPixel(j, i, Color.FromArgb(254, 254, 254));
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < 28; i++) // zwei for schleifen, um alle 28x28 Pixel abzugehen
+            {
+                for (int j = 0; j < 28; j++)//Die Grundidde hinter dem durschnitt ausrechnen ist, dass für einen Bereich alle Rot Werte zusammenaddiert werden und dann  
+                { // durch die Anzahl der Pixel im Bereich dividiert wird, so erhält man den Durschnittswert. Da es sich um Schwarz Weiß bilder handelt ist der Rot, Grün und Blau anteil der gleiche. So muss man nur einen Berechnen
+                    int R = bitmap.GetPixel(i * 12, j * 12).R; // Anfangspixel wirt intialiesert und direkt festgelegt, sodass später mit += hinzuaddiert werden kann
+
+                    for (int k = 0; k < 12; k++)
+                    {
+                        for (int l = 0; l < 12; l++)
+                        {
+                            R += bitmap.GetPixel(k + i * 12, l + j * 12).R; //Alle Rotwerte der 12x12 Pixel werden zusammengezählt
+                        }
+                    }
+
+                    Color c = new Color(); //Farbe wird intialiesert für die Pixel des verpixelten Bild.
+                    c = Color.FromArgb((int)(R / 144), (int)(R / 144), (int)(R / 144)); //Durchschnitt wird berechenet der vorhin zusammengefasstenwerte indem durch die Anzahl der Pixel 12x12 dividiert wird. Anschließend wird der Nachkommaanteil abgeblockt mit (int)
+
+                    for (int k = 0; k < 12; k++) //Die 12x12 Pixel werden auf die neue Pixelbmp gezeichnet.
+                    {
+                        for (int l = 0; l < 12; l++)
+                        {
+                            Pixelbmp.SetPixel(k + 12 * i, l + 12 * j, c);
+                        }
+                    }
+
+                    if (Math.Abs(1 - (Convert.ToDouble(R) / 144 / 255)) < 0.01) // Bei der Division kommt ein minimaler Betrag heraus bei weißem Pixel. Sollte der Auftreten, so wir er hier nullgesetzt.
+                    {
+                        Pixelarray[j * 28 + i] = 0;
+                    }
+                    else
+                    {
+                        Pixelarray[j * 28 + i] = Math.Abs(1 - (Convert.ToDouble(R) / 144 / 255)); // der Pixelwert (1=Schwarz, 0 = Weiß wird in eine Listenarray gespeichert)
+                    }
+                }
+            }
+            pictureBox1.Image = Pixelbmp; // Leinwand soll verpixeltes Bild annehmen
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            NN.neutrainieren();
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            double[] Ergbnis = new double[784];
+            Ergbnis = NN.Dreammode((int)numericUpDown1.Value);
+            for (int i = 0; i < 28; i++)
+            {
+                for (int j = 0; j < 28; j++)
+                {
+                    for (int k = 0; k < 12; k++)
+                    {
+                        for (int l = 0; l < 12; l++)
+                        {
+                            bitmap.SetPixel(j*12 +k, i*12+l, Color.FromArgb((int)(Ergbnis[i * 28 + j] * 255), (int)(Ergbnis[i * 28 + j] * 255), (int)(Ergbnis[i * 28 + j] * 255)));
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;
+            Pixelarray  = Ergbnis;
+            Netzabfrage();
+        }
+        private void Netzabfrage()
+        {
+            Vector<double> Ergebnis = NN.abfrage(Pixelarray);
+            int a = 9;
+            foreach (ProgressBar pgb in this.Controls.OfType<ProgressBar>()) //geht durch alle vorhandenen Progressbar durch und überträgt das Ergebnis mitder Zöhlervariable a. Die Mehtode ist etwas risky, da bei einer foreach Schleife die Reihenfogle auch falsch sein. Es wurde deswegen auch getestet, dass es richtig funktioniert. 
+            {
+                pgb.Value = (int)(Ergebnis[a] * 100);
+                a--;
+            }
+            int[] ea = new int[10] { progressBar1.Value, progressBar2.Value, progressBar3.Value, progressBar4.Value, progressBar5.Value, progressBar6.Value, progressBar7.Value, progressBar8.Value, progressBar9.Value, progressBar10.Value };
+            label1.Text = Convert.ToString(ea.ToList().IndexOf(ea.Max()));
+        }
+    }
+}
